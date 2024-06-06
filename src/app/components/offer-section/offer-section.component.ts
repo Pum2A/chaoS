48986302edcee3b999553a69623ad942076cfb59
Dominik Gadzina <epros98@gmail.com>
2024-06-07 00:28:18 +0200

    changed schema of offer-section, colors to fix

--- src/app/components/offer-section/offer-section.component.ts
+++ src/app/components/offer-section/offer-section.component.ts
@@ -30,76 +30,123 @@ import { heroUsers } from '@ng-icons/heroicons/outline';
       <p class="electronic-offer-text">Our products</p>
       <div class="blocks-wrapper">
         <div class="block">
-          <ng-icon name="bootstrapLaptopFill"></ng-icon>
-          <h5>Laptops</h5>
-
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit, quidem!
-          </p>
-          <div class="a-cont">
-            <a>Check more</a>
+          <div class="title-block">
+            <ng-icon name="bootstrapLaptopFill"></ng-icon>
+            <h5>Laptops</h5>
+          </div>
+          <div class="description-block">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Suscipit, quidem! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Suscipit, quidem!
+            </p>
+          </div>
+          <div class="end-block">
+            <div class="a-cont">
+              <a>Check more</a>
+            </div>
           </div>
         </div>
         <div class="block">
-          <ng-icon name="bootstrapPhoneFill"></ng-icon>
-          <h5>Phones</h5>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit, quidem!
-          </p>
-          <div class="a-cont">
-            <a>Check more</a>
+          <div class="title-block">
+            <ng-icon name="bootstrapPhoneFill"></ng-icon>
+            <h5>Phones</h5>
+          </div>
+          <div class="description-block">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Suscipit, quidem! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Suscipit, quidem!
+            </p>
+          </div>
+          <div class="end-block">
+            <div class="a-cont">
+              <a>Check more</a>
+            </div>
           </div>
         </div>
         <div class="block">
-          <ng-icon name="bootstrapHeadphones"></ng-icon>
-          <h5>Headphones</h5>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit, quidem!
-          </p>
-          <div class="a-cont">
-            <a>Check more</a>
+          <div class="title-block">
+            <ng-icon name="bootstrapHeadphones"></ng-icon>
+            <h5>Headphones</h5>
+          </div>
+          <div class="description-block">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Suscipit, quidem! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Suscipit, quidem!
+            </p>
+          </div>
+          <div class="end-block">
+            <div class="a-cont">
+              <a>Check more</a>
+            </div>
           </div>
         </div>
         <div class="block">
-          <ng-icon name="bootstrapTerminalFill"></ng-icon>
-          <h5>Computers</h5>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit, quidem!
-          </p>
-          <div class="a-cont">
-            <a>Check more</a>
+          <div class="title-block">
+            <ng-icon name="bootstrapTerminalFill"></ng-icon>
+            <h5>Computers</h5>
+          </div>
+          <div class="description-block">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Suscipit, quidem! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Suscipit, quidem!
+            </p>
+          </div>
+          <div class="end-block">
+            <div class="a-cont">
+              <a>Check more</a>
+            </div>
           </div>
         </div>
         <div class="block">
-          <ng-icon name="bootstrapHouseDoorFill"></ng-icon>
-          <h5>House</h5>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit, quidem!
-          </p>
-          <div class="a-cont">
-            <a>Check more</a>
+          <div class="title-block">
+            <ng-icon name="bootstrapHouseDoorFill"></ng-icon>
+            <h5>House</h5>
+          </div>
+          <div class="description-block">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Suscipit, quidem! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Suscipit, quidem!
+            </p>
+          </div>
+          <div class="end-block">
+            <div class="a-cont">
+              <a>Check more</a>
+            </div>
           </div>
         </div>
         <div class="block">
-          <ng-icon name="bootstrapEmojiSunglassesFill"></ng-icon>
-          <h5>Accesories</h5>
-          <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit,
-            quidem! Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-            Suscipit, quidem!
-          </p>
-          <div class="a-cont">
-            <a>Check more</a>
+          <div class="title-block">
+            <ng-icon name="bootstrapEmojiSunglassesFill"></ng-icon>
+            <h5>Accesories</h5>
+          </div>
+          <div class="description-block">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Suscipit, quidem! Lorem, ipsum dolor sit amet consectetur
+              adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor sit amet
+              consectetur adipisicing elit. Suscipit, quidem! Lorem, ipsum dolor
+              sit amet consectetur adipisicing elit. Suscipit, quidem!
+            </p>
+          </div>
+          <div class="end-block">
+            <div class="a-cont">
+              <a>Check more</a>
+            </div>
           </div>
         </div>
       </div>
